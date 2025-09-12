57e2289e777b3271c3934b13037580e31bb30589
Austin Cronin <austin.cronin3d@gmail.com>
2025-09-11 19:32:57 -0500

    initial timezones.html

--- src/time_warp/app.py
+++ src/time_warp/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        return redirect("/timezones")
+    else:
+        return render_template("index.html")
