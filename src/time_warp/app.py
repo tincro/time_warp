46392cb2782af5585af71b74c3e7f63e7a8f97f8
Austin Cronin <austin.cronin3d@gmail.com>
2025-09-16 16:19:34 -0400

    Style for search box. Added 404

--- src/time_warp/app.py
+++ src/time_warp/app.py
@@ -40,4 +40,9 @@ def search():
      options = time_warp.getNewZonesFromSearch(query)
 
      return render_template("search.html", zones=zones, others=options, q=query)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+     return render_template('404.html'), 404
     
